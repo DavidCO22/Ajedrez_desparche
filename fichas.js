3281367fe774d3059d0cb8f6ac7a8e0595d050ba
DavidCO22 <106116047+DavidCO22@users.noreply.github.com>
2023-08-30 08:45:39 -0500

    Update fichas.js

--- fichas.js
+++ fichas.js
@@ -1,5 +1,37 @@
 peon = {
 pasos : 1,
-direccion : 'directo'
+direccion : 'directo',
+imagen : "/img/peon_"
 }
+
+rey = {
+    pasos : 1,
+    direccion : 'all',
+    imagen : "/img/rey_"
+}
+
+reina = {
+    pasos : 1,
+    direccion : 'all',
+    imagen : "/img/reina_"
+}
+
+alfil = {
+    pasos : 'all',
+    direccion : 'diagonal',
+    imagen : "/img/alfil_"
+}
+
+caballo = {
+    pasos : 'L',
+    direccion : 'L',
+    imagen : "/img/caballo_"
+}
+
+torre = {
+    pasos : 'all',
+    direccion : 'directo',
+    imagen : "/img/torre_"
+}
+
 
