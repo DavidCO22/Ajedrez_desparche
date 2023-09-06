94dfb9eee4c2c65aaa0e1941382d40e78a6bf13c
DavidCO22 <106116047+DavidCO22@users.noreply.github.com>
2023-09-06 15:59:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-let a8 = document.getElementById("a8")
+/*************************************Instanciamiento con HTML objects*****************************************************************/
+
+let chest = {}
+
+for(i=97;i<=104;i++){
+    for(j=1;j<=8;j++){
+        chest[String.fromCharCode(i)+j] = document.getElementById(String.fromCharCode(i)+j)
+    }
+}
+
+/*************************************************************************************************************************************/
+
+eq_w = createam('b')
+eq_b = createam('n')
+
+console.log(eq_w)
+
+startpos(eq_w)
+startpos(eq_b)
