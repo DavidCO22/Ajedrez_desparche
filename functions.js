6823db2a552bb039792244e3f3c7ce56fa96551f
francis-roa <roabernalf@gmail.com>
2023-09-10 15:37:15 -0500

    torre
    se colorea movimiento vertical de la torre

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@ function createam(x){
 
     eq={
 
-        'torre1':new ficha(x,'t',`/img/torre_${x}.png`,true,`${x == 'b'? 'a8' : 'h8'}`),
+        'torre1':new ficha(x,'t',`/img/torre_${x}.png`,true,`${x == 'b'? 'c4' : 'h8'}`),
         'caballo1':new ficha(x,'c',`/img/caballo_${x}.png`,true,`${x == 'b'? 'a7' : 'h7'}`),
         'alfil1':new ficha(x,'a',`/img/alfil_${x}.png`,true,`${x == 'b'? 'a6' : 'h6'}`),
         'rey':new ficha(x,'r',`/img/rey_${x}.png`,true,`${x == 'b'? 'a5' : 'h5'}`),
@@ -99,6 +99,7 @@ function actionAssign(dicc_eq,eq){
 function castillo(pieza){
 
     console.log("esta atacando un castillo")
+    movement(pieza)
 
 }
 
@@ -135,4 +136,97 @@ function peon(pieza){
     
     chest[mov].style.backgroundColor = "RGBA(200,250, 0, 0.6)"
     pieza_atacante = pieza
+}
+
+function movement(pieza){
+    var color = "RGBA(200,250, 0, 0.6)"
+
+    switch(pieza.tipo){
+
+        case 't':
+            let mov = pieza.position
+            let limit
+            let j=1
+            for(let i=1;i<=4;i++){
+                console.log(i)
+                mov = pieza.position
+                switch(j){
+                    case 1:
+                        limit = parseInt(mov[1],10)+1
+                        mov=mov[0]+(parseInt(mov[1],10)+1)
+                        while(!(chest[mov].hasChildNodes())){
+                            chest[mov].style.backgroundColor = color
+                            console.log(mov,limit,j)
+                            limit++
+                            if(limit==9){
+                                break;
+                            }
+                            else
+                            {mov=mov[0]+(parseInt(mov[1],10)+1)}
+                        }
+                        break;
+                    
+                    case 2:
+                        limit = parseInt(mov[1],10)-1
+                        mov=mov[0]+(parseInt(mov[1],10)-1)
+                        while(!(chest[mov].hasChildNodes())){
+                            chest[mov].style.backgroundColor = color
+                            console.log(mov,limit,j)
+                            limit--
+                            if(limit==0){
+                                break;
+                            }
+                            else
+                            {mov=mov[0]+(parseInt(mov[1],10)-1)}
+                        }
+                        break;
+                    
+                    case 3:
+                        mov=pieza.position
+                        const letras=['a','b','c','d','e','f','g','h'];
+                        console.log(letras.findIndex(mov[0]))
+                        
+                        /*while(!(chest[pieza.position].hasChildNodes())){
+                            chest[mov].style.backgroundColor = color
+                            mov=letras.findIndex(mov[0]) +mov[1]
+                        }*/
+                        break;
+                    
+                    /*case 4:
+                        while(chest[pieza.position].hasChildNodes()){
+                            chest[mov].style.backgroundColor = color
+                            mov=String.fromCharCode(mov[0].codePointAt(0)+1)
+                        }
+                        break;*/
+                    default:
+                        console.log("No implementado")
+                }
+                j++
+            }
+            break;
+
+        case 'c':
+            caballo(pieza)
+            break;
+
+        case 'a':
+            alfil(pieza)
+            break;
+        
+        case 'q':
+            reina(pieza)
+            break;
+
+        case 'r':
+            rey(pieza)
+            break;
+
+        case 'p':
+            peon(pieza)
+            break;
+
+        default:
+            console.log("No implementado")
+
+    }
 }
