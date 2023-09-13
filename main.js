8c0c33737fabce2029fc127ff6a7469cc4e8e03f
DavidCO22 <106116047+DavidCO22@users.noreply.github.com>
2023-09-12 20:11:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let pieza_atacante;
 let tablero = document.createElement("table")
 let casilla,fila;
 let color = true;
+let cas_color;
 
 for(j=1;j<=8;j++){
 
@@ -41,17 +42,49 @@ eq_b = createam('n')
 dicc_img_w = startpos(eq_w)
 dicc_img_b = startpos(eq_b)
 
-move(dicc_img_w,dicc_img_b)
-
 console.log(dicc_img_w)
 
+//move(dicc_img_w,dicc_img_b)
+
+Object.entries(chest).forEach(([key,value])=>{
+
+    value.addEventListener('click', ()=>{
+
+        if(value.style.backgroundColor === 'rgba(200, 250, 0, 0.4)'){
+
+            console.log('ficha se movio aqui')
+            //pieza_atacante.color == 'b'? delete dicc_img_w[pieza_atacante.nombre+'_'+pieza_atacante.color] : delete dicc_img_b[pieza_atacante.nombre+'_'+pieza_atacante.color]
+            console.log(value)
+            value.append(pieza_atacante.color == 'b'? dicc_img_w[pieza_atacante.nombre+'_'+pieza_atacante.color] : dicc_img_b[pieza_atacante.nombre+'_'+pieza_atacante.color])
+            //chest[pieza_atacante.position].removeChild(chest[pieza_atacante.position].firstChild)
+            console.log(dicc_img_w[pieza_atacante.nombre+'_'+pieza_atacante.color])
+            value.style.backgroundColor = 'rgba(200, 250, 0, 0.4)'
+            recolorear()
+
+            if(pieza_atacante.color=='b'){
+
+                eq_w[pieza_atacante.nombre].position = key
+                
+            }
+            else{
+
+                eq_b[pieza_atacante.nombre].position = key
+
+            }
+            /* let img_actual = pieza_atacante.color == 'b'? dicc_img_w[pieza_atacante.nombre+'_'+pieza_atacante.color] : dicc_img_b[pieza_atacante.nombre+'_'+pieza_atacante.color] */
+            /* let imag = document.createElement("img")
+            imag.src = pieza_atacante.imagen
+            imag.id = pieza_atacante.nombre+'_'+pieza_atacante.color */
+            //chest[key].append(img_actual)
+            
+        }
+    })
+})
+
 //asignacion de eventos
 actionAssign(dicc_img_w,eq_w)
 actionAssign(dicc_img_b,eq_b)
 
 
 
-//asignacion de eventos
-actionAssign(dicc_img_w,eq_w)
-actionAssign(dicc_img_b,eq_b)
 
